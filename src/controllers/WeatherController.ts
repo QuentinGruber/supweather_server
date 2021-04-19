2c521179bd5a845572e007b66917bef9447e6358
Quentin Gruber <quentingruber@gmail.com>
2021-04-19 19:12:38 +0200

    GetCityWeather

--- src/controllers/WeatherController.ts
+++ src/controllers/WeatherController.ts
@@ -1,5 +1,24 @@
-import {  } from "../types/auth";
 import { ApiResponse } from "../types/shared";
+import axios from "axios";
 
+const GetCityWeather = async function (req: any): Promise<ApiResponse> {
+  console.log(req.query)
+  const {cityId} = req.query
+  try {
+    const result = await axios.get(`https://api.openweathermap.org/data/2.5/weather?id=${cityId}&appid=${process.env.OPENWEATHER_APIKEY}`)
+    return {
+      code: 200,
+      data: result.data,
+    };
+  } catch (error) {
+    console.log(error.response)
+    const { cod , message } = error.response.data
+    return {
+      code: cod,
+      data: message,
+    };
+  }
 
-export {};
+};
+
+export { GetCityWeather };
--- src/routes/weather.ts
+++ src/routes/weather.ts
@@ -1,7 +1,12 @@
 import express from "express";
 const router = express.Router();
 import { ApiResponse } from "types/shared";
-import {} from "../controllers/WeatherController";
+import { GetCityWeather } from "../controllers/WeatherController";
 import { isAuthenticated } from "../utils";
+
+router.get("/", async function (req, res) {
+  const result: ApiResponse = await GetCityWeather(req);
+  res.status(result.code).send({ data: result.data });
+});
 
 export { router };
